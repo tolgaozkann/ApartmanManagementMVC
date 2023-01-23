951e96970e6d696377554ccf845ccfb27c6dffd3
tolgaozkann <ozkanozkan3233@gmail.com>
2023-01-23 21:57:26 +0300

    entities added for person-role module

--- src/ApartmentManagementMVC/Entity/PersonRole.java
+++ src/ApartmentManagementMVC/Entity/PersonRole.java
@@ -4,10 +4,63 @@
  */
 package ApartmentManagementMVC.Entity;
 
+import java.util.Date;
+
 /**
  *
  * @author ozkan
  */
 public class PersonRole {
+    private int personId;
+    private int roleId;
+    private Date startingDate;
+    private Date EndingDate;
+
+    public PersonRole() {
+    }
+
+    public PersonRole(int personId, int roleId, Date startingDate, Date EndingDate) {
+        this.personId = personId;
+        this.roleId = roleId;
+        this.startingDate = startingDate;
+        this.EndingDate = EndingDate;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public Date getStartingDate() {
+        return startingDate;
+    }
+
+    public Date getEndingDate() {
+        return EndingDate;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public void setStartingDate(Date startingDate) {
+        this.startingDate = startingDate;
+    }
+
+    public void setEndingDate(Date EndingDate) {
+        this.EndingDate = EndingDate;
+    }
+
+    @Override
+    public String toString() {
+        return  personId + ", " + roleId + ", " + startingDate + ", " + EndingDate;
+    }
     
 }
--- src/ApartmentManagementMVC/Entity/Role.java
+++ src/ApartmentManagementMVC/Entity/Role.java
@@ -9,5 +9,48 @@ package ApartmentManagementMVC.Entity;
  * @author ozkan
  */
 public class Role {
+    private int id;
+    private String name;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Role(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
+    public Object getByName(String field){
+        switch(field)
+        {
+            case "Id": return id;
+            case "Name": return name;
+            default: return null;
+        }
+    }
+    
+    @Override
+    public String toString(){
+        return id + ", " + name;
+    }
 }
